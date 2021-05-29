153646767c69f8564c86db1eaf937b515853215a
youngkwon02 <youngkwon02@naver.com>
2021-05-30 00:51:27 +0900

    doc : update gitignore

--- MOIZA/MOIZA/wsgi.py
+++ MOIZA/MOIZA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MOIZA project.
+WSGI config for moiza project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MOIZA.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moiza.settings')
 
 application = get_wsgi_application()
