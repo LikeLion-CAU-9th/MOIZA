2cd7142c0292103d1d2cc04105642ba88c2a4865
youngkwon02 <youngkwon02@naver.com>
2021-05-30 01:59:29 +0900

    feat : login view after stash

--- MOIZA/MOIZA/settings.py
+++ MOIZA/MOIZA/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'account/static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
