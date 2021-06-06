a6848c9ec1127d1a81ddbde9f6eeaf50b56b3f47
Harin Kim <harin14@naver.com>
2021-06-06 22:36:03 +0900

    feat: before pull

--- moiza/opinion/urls.py
+++ moiza/opinion/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
   path('mainpage/', views.mainpage_view, name="mainpage"),
+  path('suggestion/', views.suggestion_view, name="suggestion"),
 ]
--- moiza/opinion/views.py
+++ moiza/opinion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def mainpage_view(request):
+  return render(request, 'mainpage.html')
+
+def suggestion_view(request):
+  return render(request, 'suggestion.html')
