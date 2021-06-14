a17fd4468cc6110b344a297981e10c209a02c17a
youngkwon02 <youngkwon02@naver.com>
2021-06-14 17:16:08 +0900

    refactor : following new design

--- moiza/account/views.py
+++ moiza/account/views.py
@@ -46,15 +46,15 @@ def signup_action(request):
     return HttpResponseBadRequest('This view can not handle method {0}'.format(request.method), status=405)
   data = request.POST
   if validate_id(request, data.get('user_id', False)) == False:
-    return render(request, 'signup.html', {'msg_id': '이미 가입된 아이디입니다.'})
+    return render(request, 'signup.html')
   if data.get('user_pw', False) != data.get('user_pwck', False):
-    return render(request, 'signup.html', {'msg_pw': '비밀번호가 일치하지 않습니다.'})
+    return render(request, 'signup.html')
   if validate_mail(request, data.get('user_email', False)) == False:
-    return render(request, 'signup.html', {'msg_mail': '이미 가입된 이메일입니다.'})
+    return render(request, 'signup.html')
   HashedPasswordObj =hashlib.sha1(data.get('user_pw', False).encode('UTF-8'))
   HashedPassword = HashedPasswordObj.hexdigest()
   user_info.objects.create(user_id=data.get('user_id', False), user_pw=HashedPassword, user_email=data.get('user_email', False), user_name=data.get('user_name', False))
-  return render(request, 'login.html', {'msg': '회원가입 성공!'})
+  return render(request, 'login.html')
 
 
 def validate_id(request, id):
