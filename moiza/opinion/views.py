7d46610bc8f7311439c024cd3285bb095d369214
Harin Kim <harin14@naver.com>
2021-06-19 20:19:21 +0900

    feat:create frame_decision&completes

--- moiza/opinion/views.py
+++ moiza/opinion/views.py
@@ -5,3 +5,12 @@ def mainpage_view(request):
 
 def suggestion_view(request):
   return render(request, 'suggestion.html')
+
+def topic_complete_view(request):
+  return render(request, 'topic_complete.html')
+
+def decision_complete_view(request):
+  return render(request, 'decision_complete.html')
+
+def decision_view(request):
+  return render(request, 'decision.html')
