269f9faf1bb6a220188f497614568d45aba6f957
youngkwon02 <youngkwon02@naver.com>
2021-06-23 03:09:16 +0900

    feat : result-bottomChoice

--- moiza/opinion/static/js/result.js
+++ moiza/opinion/static/js/result.js
@@ -1,17 +1,3 @@
-window.onload = () => {
-  // Need to be refactor when the data is given
-  let gage = document.querySelectorAll('.gage');
-  percent = (3*160)/7; // Need Saciling!!
-  gage[0].style.width = String(percent) + "%";
-  percent = (1*160)/7; // Need Saciling!!
-  gage[1].style.width = String(percent) + "%";
-  percent = (1*160)/7; // Need Saciling!!
-  gage[2].style.width = String(percent) + "%";
-  percent = (2*160)/7; // Need Saciling!!
-  gage[3].style.width = String(percent) + "%";
-
-}
-
 const bottomSheetSwitch = () => {
   const sheet = document.querySelectorAll('.bottom-sheet')[0];
   const container = document.querySelectorAll('.bottom-sheet-container')[0];
@@ -26,6 +12,62 @@ const bottomSheetSwitch = () => {
   return 0;
 }
 
-const bottomChoice = () => {
+const bottomChoice = (num) => {
+  const choice = document.querySelectorAll('.bottom-choice')
+  const select = choice[num];
+  for(let i = 0; i < choice.length; i++) {
+    console.log(i);
+    removeClass('.bottom-choice', i, 'selected');
+  }
+  select.className += " selected"
+}
 
+const removeClass = (JSpath, index, className) => {
+  const target = document.querySelectorAll(JSpath)[index];
+  console.log("remove" + index)
+  const classList = target.className.split(' ');
+  let newClassName = "";
+  for(let i = 0; i < classList.length; i++) {
+    if(classList[i] != className) {
+      newClassName += (classList[i] + " ");
+    }
+  }
+  newClassName = newClassName.trimEnd();
+  target.className = newClassName;
+}
+
+const choiceClickHandler0 = () => {
+  bottomChoice(0);
+}
+
+const choiceClickHandler1 = () => {
+  bottomChoice(1);
+}
+
+const choiceClickHandler2 = () => {
+  bottomChoice(2);
+}
+
+const choiceClickHandler3 = () => {
+  bottomChoice(3);
+}
+
+
+window.onload = () => {
+  // Need to be refactor when the data is given
+  let gage = document.querySelectorAll('.gage');
+  percent = (3*160)/7; // Need Saciling!!
+  gage[0].style.width = String(percent) + "%";
+  percent = (1*160)/7; // Need Saciling!!
+  gage[1].style.width = String(percent) + "%";
+  percent = (1*160)/7; // Need Saciling!!
+  gage[2].style.width = String(percent) + "%";
+  percent = (2*160)/7; // Need Saciling!!
+  gage[3].style.width = String(percent) + "%";
+
+  let choice = document.querySelectorAll('.bottom-choice');
+  choice[0].addEventListener("click", choiceClickHandler0, false);
+  choice[1].addEventListener("click", choiceClickHandler1, false);
+  choice[2].addEventListener("click", choiceClickHandler2, false);
+  choice[3].addEventListener("click", choiceClickHandler3, false);
 }
