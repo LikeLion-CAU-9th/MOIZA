f530b80d50710d64c037b1f1fe0fb4e753226b00
youngkwon02 <youngkwon02@naver.com>
2021-06-25 01:40:07 +0900

    fix : splash display prop after disappear

--- moiza/account/static/js/splash.js
+++ moiza/account/static/js/splash.js
@@ -2,4 +2,7 @@ window.onload = () => {
   setTimeout(() => {
     document.querySelector('.splash-container').style.opacity = '0';
   }, 1600)
+  setTimeout(()=> {
+    document.querySelector('.splash-container').style.display = 'none';
+  },3000)
 }
