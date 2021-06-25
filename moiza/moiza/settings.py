5df088d12288be1fa2cb7a646519a2c1a13cbd93
youngkwon02 <youngkwon02@naver.com>
2021-06-25 16:15:15 +0900

    feat : mysql set

--- moiza/moiza/settings.py
+++ moiza/moiza/settings.py
@@ -1,10 +1,11 @@
 import os, json
 from django.core.exceptions import ImproperlyConfigured
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -75,10 +76,16 @@ WSGI_APPLICATION = 'moiza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MOIZA_DB',
+        'HOST': get_secret("HOST"),
+        'PORT': get_secret("PORT"),
+        'USER': get_secret("USER"),
+        "PASSWORD": get_secret("PASSWORD")
     }
 }
 
