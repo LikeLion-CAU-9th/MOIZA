3c024621d4180711764d3cb9edd870c5f2bc01c5
youngkwon02 <youngkwon02@naver.com>
2021-06-26 16:39:53 +0900

    doc : update gitignore

--- moiza/account/static/js/login.js
+++ moiza/account/static/js/login.js
@@ -4,7 +4,7 @@ const loginAction = () => {
   result = AjaxCall('../login-action/', {"email": userEmail, "pw": userPw}, 'GET', false);
   
   if(result) {
-    location.href = "../success/";
+    location.href = "../mainpage/";
   }else{
     // Else로 처리 안하면 success로 넘어갈 때 모달 나타남
     modal_on('login-fail');
