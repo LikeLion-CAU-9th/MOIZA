e1d26ae577a722a62c44361fc8d3dc7b164b4a3f
youngkwon02 <youngkwon02@naver.com>
2021-06-28 02:03:51 +0900

    refactor : wordCount-remove printing log

--- moiza/opinion/static/js/wordCount.js
+++ moiza/opinion/static/js/wordCount.js
@@ -1,7 +1,6 @@
 function wordCount() {
     let reason_content = document.getElementById('reason-content');
     let length = reason_content.value.length;
-    console.log(length)
     if (length >= 15) {
         document.querySelector("#submit-button input").style.pointerEvents = "all"
         document.querySelector("#submit-button input").style.backgroundColor = "#7150e7"
