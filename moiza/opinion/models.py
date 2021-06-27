9a1f798d8a4748e6133d0d0c23bb7ee42f6fce11
youngkwon02 <youngkwon02@naver.com>
2021-06-27 22:45:04 +0900

    refactor : opinion/models.py

--- moiza/opinion/models.py
+++ moiza/opinion/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from account.models import User_info
 
 
 class Group_info(models.Model):
   group_seq = models.AutoField(primary_key=True)
-  owner_seq = models.ForeignKey("User_info", related_name="Group", on_delete=models.CASCADE, db_column="owner")
+  owner = models.ForeignKey(User_info, related_name="Group", on_delete=models.CASCADE, db_column="owner")
   name = models.TextField(null=False)
   create_date = models.DateField(auto_now_add=True, verbose_name="create_date")
   
 
 class Membership(models.Model):
-  group_seq = models.ForeignKey("Group_seq", related_name="Membership", on_delete=models.CASCADE, db_column="group_seq")
-  user_seq = models.ForeignKey("User_seq", related_name="Membership", on_delete=models.CASCADE, db_column="user_seq")
+  group = models.ForeignKey(Group_info, related_name="Membership", on_delete=models.CASCADE, db_column="group_seq")
+  user = models.ForeignKey(User_info, related_name="Membership", on_delete=models.CASCADE, db_column="user_seq")
   join_date = models.DateField(auto_now_add=True, verbose_name="create_date")
   auth = models.CharField(max_length=3, null=True, default=None)
